f6d06d8cbaf89e286b2d6bff0dde4e98cc467325
f1sherFM <kirillka229top@gmail.com>
2026-02-28 19:52:25 +0500

    Add non-namespaced category_create URL alias for compatibility

--- skill_blog/urls.py
+++ skill_blog/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from blog.views import CategoryCreateView
 
 # Кастомные handlers нужны, чтобы 403/404 выглядели единообразно с UI проекта.
 handler403 = "skill_blog.views.permission_denied_view"
@@ -9,6 +10,8 @@ handler404 = "skill_blog.views.page_not_found_view"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # Совместимость со старыми шаблонами без namespace: {% url 'category_create' %}.
+    path("category/create/", CategoryCreateView.as_view(), name="category_create"),
     path("", include("accounts.urls")),
     path("", include("blog.urls")),
 ]
